244326aa56cac04c69c7f8d023ebff3d27c4b1bb
Kevin Dietze <kdietze3@gmail.com>
2013-02-14 00:11:13 -0500

    Basic Parsing done for the top 35 teams. TODO: Loop over the other tables

--- ncaa_score_scraper.py
+++ ncaa_score_scraper.py
@@ -1,7 +1,6 @@
 import re
 import httplib
-from collections import namedtuple
-
+from bs4 import BeautifulSoup
 
 
 connection = httplib.HTTPConnection("www.d1baseball.com")
@@ -9,14 +8,23 @@ connection.request("GET", "/2013/daily/0215.htm")
 response = connection.getresponse()
 xml = response.read()
 
-conferenceArray = re.findall(r'<h4><center><b><a\sname=.+?</td></tr></table></td>\s+</tr></table>',xml,re.DOTALL)
+soup = BeautifulSoup(xml)
 
-for i in range(0,len(conferenceArray)):
-    conferenceArray[i] = re.sub(r'<.*?>',"",conferenceArray[i])
-    conferenceArray[i] = re.sub(r'\s\s+',"\n",conferenceArray[i])
-    conferenceArray[i] = conferenceArray[i].split(r'\n')
+##print(soup.prettify())
 
+
+##Deemed not effective due to additional game location notes only present in some games.
+##conferenceArray = re.findall(r'<h4><center><b><a\sname=.+?</td></tr></table></td>\s+</tr></table>',xml,re.DOTALL)
+##
 ##for i in range(0,len(conferenceArray)):
-##    for j in range(0,conferenceArray[i]):
-##        gameArray[j,0]
-        
+##    conferenceArray[i] = re.sub(r'<.*?>',"",conferenceArray[i])
+##    conferenceArray[i] = re.sub(r'\s\s+',"\n",conferenceArray[i])
+##    conferenceArray[i] = re.split('\n|\t',conferenceArray[i])
+
+##Just finds first table
+table = soup.find('table')
+
+rows = table.findAll('tr')
+data = [[td.findChildren(text=True) for td in tr.findAll("td")] for tr in rows]
+data = [[u"".join(d).strip() for d in l] for l in data]
+    
